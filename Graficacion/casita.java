78fa0a04525b6de69941e4ef0d854b7e9da05e67
MacbethCarrillo <31285010+MacbethCarrillo@users.noreply.github.com>
2024-02-26 15:33:21 -0700

    Add files via upload

---
+++ Graficacion/casita.java
@@ -0,0 +1,210 @@
+/*
+* -GRAFICACION-
+* 
+* ALUMNO: MACBETH ADOLFO CARRILLO IBARRA
+* 
+* NO.CONTROL: 19170557
+* 
+* SEMESTRE: 6
+* 
+* PROFESOR: CESAR ALFREDO SANCHEZ BELTRAN
+* 
+*/
+package pakete;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+import javax.swing.JFrame;
+public class casita extends JFrame {
+ public casita() {
+
+super("Formas básicas");
+ setSize(800, 700);
+ setVisible(true);
+ }
+ public void paint(Graphics g) {
+ super.paint(g);
+ Color negro = new Color(0, 0, 0);
+ Color blanco = new Color(255, 255, 255);
+ Color verde = new Color(102, 255, 102);
+ Color amarillo = new Color(255, 204, 0);
+ Color azul = new Color(0, 0, 255);
+ Color rosa = new Color(255, 204, 229);
+ Color cafe = new Color(153, 102, 0);
+ Color naranja = new Color(255, 153, 0);
+ g.setColor(azul);
+ g.fillRect(0, 0, 800, 400);
+ g.setColor(cafe);
+ g.fillRect(0, 400, 800, 800);
+ g.setColor(verde);
+ g.drawLine(0, 700, 1000, 700);
+ g.setColor(verde);
+ g.drawLine(0, 690, 1000, 690);
+ g.setColor(verde);
+ g.drawLine(0, 685, 1000, 685);
+ g.setColor(verde);
+ g.drawLine(0, 680, 1000, 680);
+ g.setColor(verde);
+ g.drawLine(0, 675, 1000, 675);
+ g.setColor(verde);
+ g.drawLine(0, 670, 1000, 670);
+ g.setColor(verde);
+ g.drawLine(0, 665, 1000, 655);
+ g.setColor(verde);
+ g.drawLine(0, 660, 1000, 660);
+ g.setColor(verde);
+ g.drawLine(0, 655, 1000, 655);
+ g.setColor(verde);
+ g.drawLine(0, 650, 1000, 650);
+ g.setColor(verde);
+ g.drawLine(0, 645, 1000, 645);
+ g.setColor(verde);
+ g.drawLine(0, 640, 1000, 640);
+ g.setColor(verde);
+ g.drawLine(0, 635, 1000, 635);
+ g.setColor(verde);
+ g.drawLine(0, 630, 1000, 630);
+ g.setColor(verde);
+ g.drawLine(0, 625, 1000, 625);
+ g.setColor(verde);
+ g.drawLine(0, 620, 1000, 620);
+ g.setColor(verde);
+ g.drawLine(0, 615, 1000, 615);
+ g.setColor(verde);
+ g.drawLine(0, 610, 1000, 610);
+ g.setColor(verde);
+ g.drawLine(0, 605, 1000, 605);
+ g.setColor(verde);
+ g.drawLine(0, 600, 1000, 600);
+ g.setColor(verde);
+ g.drawLine(0, 595, 1000, 595);
+ g.setColor(verde);
+ g.drawLine(0, 590, 1000, 590);
+ g.setColor(verde);
+ g.drawLine(0, 585, 1000, 585);
+ g.setColor(verde);
+ g.drawLine(0, 580, 1000, 580);
+ g.setColor(verde);
+ g.drawLine(0, 575, 1000, 575);
+ g.setColor(verde);
+ g.drawLine(0, 570, 1000, 570);
+ g.setColor(verde);
+ g.drawLine(0, 565, 1000, 565);
+ g.setColor(verde);
+ g.drawLine(0, 560, 1000, 560);
+ g.setColor(verde);
+ g.drawLine(0, 555, 1000, 555);
+ g.setColor(verde);
+ g.drawLine(0, 550, 1000, 550);
+ g.setColor(verde);
+ g.drawLine(0, 545, 1000, 545);
+ g.setColor(verde);
+ g.drawLine(0, 540, 1000, 540);
+ g.setColor(verde);
+ g.drawLine(0, 535, 1000, 535);
+ g.setColor(verde);
+ g.drawLine(0, 530, 1000, 530);
+ g.setColor(verde);
+ g.drawLine(0, 525, 1000, 525);
+ g.setColor(verde);
+ g.drawLine(0, 520, 1000, 520);
+ g.setColor(verde);
+ g.drawLine(0, 515, 1000, 515);
+ g.setColor(verde);
+ g.drawLine(0, 510, 1000, 510);
+ g.setColor(verde);
+ g.drawLine(0, 505, 1000, 505);
+ g.setColor(verde);
+ g.drawLine(0, 500, 1000, 500);
+ g.setColor(verde);
+ g.drawLine(0, 495, 1000, 495);
+ g.setColor(verde);
+ g.drawLine(0, 490, 1000, 490);
+ g.setColor(verde);
+ g.drawLine(0, 485, 1000, 485);
+ g.setColor(verde);
+ g.drawLine(0, 480, 1000, 480);
+ g.setColor(verde);
+ g.drawLine(0, 475, 1000, 475);
+ g.setColor(verde);
+ g.drawLine(0, 470, 1000, 470);
+ g.setColor(verde);
+ g.drawLine(0, 465, 1000, 465);
+ g.setColor(verde);
+ g.drawLine(0, 460, 1000, 460);
+ g.setColor(verde);
+ g.drawLine(0, 455, 1000, 455);
+ g.setColor(verde);
+ g.drawLine(0, 450, 1000, 450);
+ g.setColor(verde);
+ g.drawLine(0, 445, 1000, 445);
+ g.setColor(verde);
+ g.drawLine(0, 440, 1000, 440);
+ g.setColor(verde);
+ g.drawLine(0, 435, 1000, 435);
+ g.setColor(verde);
+ g.drawLine(0, 430, 1000, 430);
+ g.setColor(verde);
+ g.drawLine(0, 425, 1000, 425);
+ g.setColor(verde);
+ g.drawLine(0, 420, 1000, 420);
+ g.setColor(verde);
+ g.drawLine(0, 415, 1000, 415);
+ g.setColor(verde);
+ g.drawLine(0, 410, 1000, 410);
+ g.setColor(verde);
+ g.drawLine(0, 405, 1000, 405);
+ g.setColor(verde);
+ g.drawLine(0, 400, 1000, 400);
+ g.setColor(amarillo);
+ g.fillOval(100, 100, 100, 100);
+ g.setColor(verde);
+ g.fillOval(595, 423, 355, 105);
+ g.setColor(azul);
+ g.fillOval(600, 425, 350, 100);
+ g.setColor(verde);
+ g.fillRect(160, 460, 100, 50);
+ g.setColor(negro);
+ g.drawRect(160, 460, 100, 50);
+ g.setColor(naranja);
+ g.fillRect(200, 460, 20, 50);
+ g.setColor(naranja);
+ int[] x = {200, 0, 400};
+ int[] y = {200, 400, 400};
+ g.fillPolygon(x, y, 3);
+ g.setColor(naranja);
+ int[] x1 = {450, 300, 600};
+ int[] y1 = {50, 400, 400};
+ g.fillPolygon(x1, y1, 3);
+ g.setColor(naranja);
+ int[] x2 = {700, 500, 850};
+ int[] y2 = {200, 400, 400};
+ g.fillPolygon(x2, y2, 3);
+ g.setColor(rosa);
+ g.fillRect(160, 400, 100, 60);
+ g.setColor(blanco);
+ g.fillRect(170, 430, 10, 10);
+ g.setColor(blanco);
+ g.fillRect(240, 430, 10, 10);
+ g.setColor(negro);
+ g.drawRect(170, 430, 10, 10);
+ g.setColor(negro);
+ g.drawRect(240, 430, 10, 10);
+
+ g.setColor(negro);
+ g.drawLine(159, 410, 260, 410);
+ g.setColor(negro);
+ g.drawLine(159, 400, 260, 400);
+ g.setColor(blanco);
+ g.fillRect(200, 430, 20, 30);
+ g.setColor(negro);
+ g.drawRect(200, 430, 20, 30);
+ g.setColor(negro);
+ g.drawRect(160, 410, 100, 50);
+ }
+ public static void main(String[] args) {
+ casita application = new casita();
+ application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ }
+
+}
