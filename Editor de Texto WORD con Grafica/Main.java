0a931301f2f18a5a77a36baba3888cbe114b5e57
MacbethCarrillo <31285010+MacbethCarrillo@users.noreply.github.com>
2024-02-26 15:45:05 -0700

    Add files via upload

---
+++ Editor de Texto WORD con Grafica/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Materia: Tópicos Avanzados de Programación 
+ * Autor Del Programa: Carrillo Ibarra Macbeth Adolfo 
+ * Horario: 08 - 09 AM
+ * Unidad 1
+ * Maestro:  Jaime Arturo Félix Medina
+ * 
+ */
+package U1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		new EditorTexto();
+
+	}
+}
